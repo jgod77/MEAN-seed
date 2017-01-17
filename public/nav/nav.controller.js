be59bf03a97b287b39b2fe6ffd1c1412724aed32
Jacob Goddard <jrglivin@gmail.com>
2017-01-16 17:23:30 -0800

    Delete old code

--- public/nav/nav.controller.js
+++ public/nav/nav.controller.js
@@ -1,14 +1,8 @@
 angular.module('NavBarCtrl', [])
   .controller('NavBarController', function($controller, Login) {
 
-    // var ModalController = $controller('ModalController', {'$scope' : '$scope'})
-    var modal = function(){
-      console.log('test')
-      // ModalController.modalOpen = true;
-    }
 
     angular.extend(this, {
-      modal : modal,
       Login : Login
     });
   });
