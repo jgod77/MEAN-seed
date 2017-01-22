df8bdbeff7964c0ba0ce112d5f2496c8652d4712
Jacob Goddard <jrglivin@gmail.com>
2017-01-21 22:18:45 -0800

    Add gulp build

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
+var ngAnnotate = require('gulp-ng-annotate');
+var ngmin = require('gulp-ngmin');
+
+
+gulp.task('sass', function() {
+  gulp.src('./styles/main.scss')
+    .pipe(sass({style: 'expanded'}))
+      .on('error', gutil.log)
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('html', function() {
+  gulp.src(['./public/**/*.html', '!public/libs/**'])  
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./public/**/*.js', ['js']);
+    gulp.watch('./styles/main.scss', ['sass']);
+});
+
+
+gulp.task('js', function () {
+  gulp.src(['./public/**/*.js', '!public/libs/**', '!public/app.js', '!public/app-routes.js'])
+    .pipe(sourcemaps.init())
+      .pipe(concat('app.js'))
+      .pipe(ngAnnotate())
+      .pipe(ngmin())
+      .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist'));
+
+    gulp.src(['./public/app.js', './public/app-routes.js'])
+    .pipe(sourcemaps.init())
+      .pipe(ngAnnotate())
+      .pipe(ngmin())
+      .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('build', ['js', 'html']);
+
+//  =-=-=-=-=-   './dist/minSafe/**/*.js'
--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/dist'));
 
 require('./app/routes')(app);
 
