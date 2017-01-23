5a90f30231f94ec955a0303158bc8d5cb9c85d89
Jacob Goddard <jrglivin@gmail.com>
2017-01-23 14:47:14 -0800

    Implement browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,17 @@ var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
 var ngAnnotate = require('gulp-ng-annotate');
 var ngmin = require('gulp-ngmin');
+var browserSync = require('browser-sync').create();
+var historyApiFallback = require('connect-history-api-fallback');
+var proxyMiddleware = require('http-proxy-middleware');
+
+
 
 gulp.task('css', function() {
   gulp.src('./public/css/style.css')
     .pipe(gulp.dest('dist/css'));
 });
+
 
 gulp.task('sass', function() {
   gulp.src('./styles/main.scss')
@@ -18,10 +24,13 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('dist'));
 });
 
+
 gulp.task('html', function() {
   gulp.src(['./public/**/*.html', '!public/libs/**'])  
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.reload({stream: true}));
 });
+
 
 gulp.task('js', function () {
   gulp.src(['./public/app.js', './public/**/*.js'])
@@ -34,11 +43,25 @@ gulp.task('js', function () {
     .pipe(gulp.dest('dist'));
 });
 
+
+gulp.task('reload',['build', 'watch'], function() {
+  var proxy = proxyMiddleware('/api', {target: 'http://127.0.0.1:8080'});
+  browserSync.init({
+    server: {
+      baseDir:['./dist']
+    },
+    middleware: [proxy, historyApiFallback()],
+    port: 1337
+  });
+});
+
+
 gulp.task('watch', function() {
     gulp.watch('./public/**/*.js', ['js']);
     gulp.watch('./public/css/main.scss', ['sass']);
     gulp.watch('./public/css/main.css', ['css']);
     gulp.watch('./public/**/*.html', ['html']);
 });
+
 
 gulp.task('build', ['js', 'html', 'css', 'sass']);
